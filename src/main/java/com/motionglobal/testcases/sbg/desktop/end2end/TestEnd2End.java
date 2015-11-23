07d3f862a5264c81a1b3463331891273dcb9b635
allenchliu <allenchliu@hotmail.com>
2015-11-23 15:22:03 +0800

    debug

--- src/main/java/com/motionglobal/testcases/sbg/desktop/end2end/TestEnd2End.java
+++ src/main/java/com/motionglobal/testcases/sbg/desktop/end2end/TestEnd2End.java
@@ -3,6 +3,8 @@ package com.motionglobal.testcases.sbg.desktop.end2end;
 import org.testng.annotations.Test;
 
 import com.motionglobal.pages.sbg.desktop.home.HomePage;
+import com.motionglobal.pages.sbg.desktop.search.ResultGrid;
+import com.motionglobal.pages.sbg.desktop.search.SearchResultItem;
 import com.motionglobal.pages.sbg.desktop.search.SearchResultPage;
 import com.motionglobal.testcases.sbg.desktop.AbstractBaseSbgDesktopTestCase;
 
@@ -15,7 +17,9 @@ public class TestEnd2End extends AbstractBaseSbgDesktopTestCase {
         homePage.header().inputSearch.sendKeys("ray ban");
         homePage.header().iconSearch.click();
         SearchResultPage searchResultPage = new SearchResultPage();
-        searchResultPage.resultGrid().getItem(0).click();
+        ResultGrid resultGrid = searchResultPage.resultGrid();
+        SearchResultItem item1 = resultGrid.getItem(0);
+        item1.click();
         // searchResultPage.header().logo.click();
         // Assert.assertTrue(homePage.isTextPresent("Ray-Ban"));
     }
