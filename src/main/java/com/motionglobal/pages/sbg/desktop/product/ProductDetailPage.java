4336994ba2c5810a23878b2dac994fbbba767e54
allenchliu <allenchliu@hotmail.com>
2015-11-23 15:47:55 +0800

    product details and cart page.

--- src/main/java/com/motionglobal/pages/sbg/desktop/product/ProductDetailPage.java
+++ src/main/java/com/motionglobal/pages/sbg/desktop/product/ProductDetailPage.java
@@ -1,10 +1,15 @@
 package com.motionglobal.pages.sbg.desktop.product;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import com.motionglobal.pages.sbg.desktop.AbstractBaseSbgDesktopPage;
 
 public class ProductDetailPage extends AbstractBaseSbgDesktopPage {
+
+    @FindBy(css = ".pro_r_buynow")
+    public WebElement btnBuyNow;
 
     public ProductDetailPage() {
         super();
--- src/main/java/com/motionglobal/testcases/sbg/desktop/end2end/TestEnd2End.java
+++ src/main/java/com/motionglobal/testcases/sbg/desktop/end2end/TestEnd2End.java
@@ -2,9 +2,9 @@ package com.motionglobal.testcases.sbg.desktop.end2end;
 
 import org.testng.annotations.Test;
 
+import com.motionglobal.pages.sbg.desktop.cart.CartPage;
 import com.motionglobal.pages.sbg.desktop.home.HomePage;
-import com.motionglobal.pages.sbg.desktop.search.ResultGrid;
-import com.motionglobal.pages.sbg.desktop.search.SearchResultItem;
+import com.motionglobal.pages.sbg.desktop.product.ProductDetailPage;
 import com.motionglobal.pages.sbg.desktop.search.SearchResultPage;
 import com.motionglobal.testcases.sbg.desktop.AbstractBaseSbgDesktopTestCase;
 
@@ -17,9 +17,10 @@ public class TestEnd2End extends AbstractBaseSbgDesktopTestCase {
         homePage.header().inputSearch.sendKeys("ray ban");
         homePage.header().iconSearch.click();
         SearchResultPage searchResultPage = new SearchResultPage();
-        ResultGrid resultGrid = searchResultPage.resultGrid();
-        SearchResultItem item1 = resultGrid.getItem(0);
-        item1.click();
+        searchResultPage.resultGrid().getItem(0).click();
+        ProductDetailPage productDetailPage = new ProductDetailPage();
+        productDetailPage.btnBuyNow.click();
+        new CartPage().btnCheckout.click();
         // searchResultPage.header().logo.click();
         // Assert.assertTrue(homePage.isTextPresent("Ray-Ban"));
     }
