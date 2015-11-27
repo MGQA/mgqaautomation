84348620428b347e18594e0483f2a1d00e39dbba
allenchliu <allenchliu@hotmail.com>
2015-11-27 09:46:05 +0800

    Added home page granify

---
+++ src/main/java/com/motionglobal/testcases/sbg/desktop/thirdpartytools/Granify.java
@@ -0,0 +1,29 @@
+package com.motionglobal.testcases.sbg.desktop.thirdpartytools;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.motionglobal.pages.sbg.desktop.home.HomePage;
+import com.motionglobal.testcases.sbg.desktop.AbstractBaseSbgDesktopTestCase;
+
+public class Granify extends AbstractBaseSbgDesktopTestCase {
+
+    @Test(groups = { "debug", "au" })
+    public void testHomepage() {
+        logger().info("started...");
+        HomePage homePage = new HomePage();
+        Assert.assertTrue(homePage.isTextPresent("{ page_type: \"home\" }"));
+    }
+
+    @Override
+    protected void initialize() {
+        driver.get("www.visiondirect.com.au");
+    }
+
+    @Override
+    protected void tearDown() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
