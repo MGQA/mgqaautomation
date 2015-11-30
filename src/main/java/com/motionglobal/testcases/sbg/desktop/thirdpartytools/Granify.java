702b0acc04e1770a87114f41e6481986f06537e3
allenchliu <allenchliu@hotmail.com>
2015-11-30 11:46:32 +0800

    FAQ and About  us

--- src/main/java/com/motionglobal/testcases/sbg/desktop/thirdpartytools/Granify.java
+++ src/main/java/com/motionglobal/testcases/sbg/desktop/thirdpartytools/Granify.java
@@ -3,6 +3,8 @@ package com.motionglobal.testcases.sbg.desktop.thirdpartytools;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.motionglobal.pages.sbg.desktop.footer.aboutus.AboutUsPage;
+import com.motionglobal.pages.sbg.desktop.footer.customercare.FaqPage;
 import com.motionglobal.pages.sbg.desktop.home.HomePage;
 import com.motionglobal.pages.sbg.desktop.product.ProductDetailPage;
 import com.motionglobal.pages.sbg.desktop.product.ProductGridPage;
@@ -71,7 +73,7 @@ public class Granify extends AbstractBaseSbgDesktopTestCase {
         Assert.assertFalse(productGridPage.isTextPresent("{ page_type: \"cart\" }"));
     }
 
-    @Test(groups = { "debug", "au" })
+    @Test(groups = { "acceptance", "au" })
     public void testSearchResultPage() {
         driver.get(
                 "http://www.visiondirect.com.au/search?keywords=rayban&searchHashcode=1445407688101619#q=rayban&page=0&minReviewsCount=0&refinements=[{%22for_sale%22%3A%221%22}]");
@@ -80,6 +82,23 @@ public class Granify extends AbstractBaseSbgDesktopTestCase {
         Assert.assertTrue(searchResultPage.isTextPresent("Granify.trackPageView("));
         Assert.assertTrue(searchResultPage.isTextPresent("{ page_type: \"search\" }"));
         Assert.assertFalse(searchResultPage.isTextPresent("{ page_type: \"cart\" }"));
+    }
+
+    @Test(groups = { "debug", "au" })
+    public void testOtherPage() {
+        driver.get("http://www.visiondirect.com.au/faq");
+        FaqPage faqPage = new FaqPage();
+        Assert.assertTrue(faqPage.isTextPresent("var GRANIFY_SITE_ID=1257;"));
+        Assert.assertTrue(faqPage.isTextPresent("Granify.trackPageView("));
+        Assert.assertTrue(faqPage.isTextPresent("{ page_type: \"other\" }"));
+        Assert.assertFalse(faqPage.isTextPresent("{ page_type: \"cart\" }"));
+
+        driver.get("http://www.visiondirect.com.au/about-us");
+        AboutUsPage aboutUsPage = new AboutUsPage();
+        Assert.assertTrue(aboutUsPage.isTextPresent("var GRANIFY_SITE_ID=1257;"));
+        Assert.assertTrue(aboutUsPage.isTextPresent("Granify.trackPageView("));
+        Assert.assertTrue(aboutUsPage.isTextPresent("{ page_type: \"other\" }"));
+        Assert.assertFalse(aboutUsPage.isTextPresent("{ page_type: \"cart\" }"));
     }
 
     @Override
