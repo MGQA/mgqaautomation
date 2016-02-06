caf1098294624c7c6d9f9ad32230d580254d5b7c
allen liu <allen.liu@motionglobal.com>
2016-02-06 12:06:00 +0800

    dashboard listener

--- src/main/java/com/motionglobal/testcases/AbstractBaseTestCase.java
+++ src/main/java/com/motionglobal/testcases/AbstractBaseTestCase.java
@@ -43,7 +43,6 @@ public abstract class AbstractBaseTestCase {
         driver = InitializeUtility.initializeDriver(CONFIG.getProperty("browser"));
         this.context = context;
         context.setAttribute(ScreenshotListener.DRIVER_ATTR + Thread.currentThread().getId(), driver);
-        context.setAttribute("driver" + Thread.currentThread().getId(), driver);
 
         initialize();
     }
