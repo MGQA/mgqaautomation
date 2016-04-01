9c257dcf69971508215c0c76f447aa344e3f3221
allen liu <allen.liu@motionglobal.com>
2016-04-01 17:32:55 +0800

    importer bat

--- src/main/java/com/motionglobal/testcases/sbg/desktop/header_footer/TestLanguageChange.java
+++ src/main/java/com/motionglobal/testcases/sbg/desktop/header_footer/TestLanguageChange.java
@@ -2,28 +2,26 @@ package com.motionglobal.testcases.sbg.desktop.header_footer;
 
 import java.util.List;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import com.motionglobal.pages.sbg.desktop.home.HomePage;
 import com.motionglobal.testcases.sbg.desktop.AbstractBaseSbgDesktopTestCase;
 
 public class TestLanguageChange extends AbstractBaseSbgDesktopTestCase {
 
-    public void testSunGlassesHeaderMenus() {
-        logger().info("started...");
-        HomePage homePage = new HomePage();
-        for (String value : homePage.header().countryIDs) {
-            homePage.header().selectCountryLanguageByIndex(value, 0);
-
-            Assert.assertArrayEquals(homePage.header().countryIDs, getCountryIDs());
-            p(driver.findElements(By.cssSelector("select.language-select option")));
-            logger().info(homePage.header().getCurrentLanguage());
-        }
-
-        // p(driver.findElements(By.cssSelector("select.country-select option")));
-    }
+    // public void testSunGlassesHeaderMenus() {
+    // logger().info("started...");
+    // HomePage homePage = new HomePage();
+    // for (String value : homePage.header().countryIDs) {
+    // homePage.header().selectCountryLanguageByIndex(value, 0);
+    //
+    // Assert.assertArrayEquals(homePage.header().countryIDs, getCountryIDs());
+    // p(driver.findElements(By.cssSelector("select.language-select option")));
+    // logger().info(homePage.header().getCurrentLanguage());
+    // }
+    //
+    // // p(driver.findElements(By.cssSelector("select.country-select option")));
+    // }
 
     public String[] getCountryIDs() {
         List<WebElement> eles = driver.findElements(By.cssSelector("#header_lang_select select.country-select option"));
